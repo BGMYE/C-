5c1901a492888c88b86b57b3386684785fbed2b6
Jinx <43530860+BGMYE@users.noreply.github.com>
2024-04-26 12:23:53 +0800

    Create 最长公共子序列.py

---
+++ 最长公共子序列.py
@@ -0,0 +1,29 @@
+def lcs(X, Y):
+    m, n = len(X), len(Y)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if X[i - 1] == Y[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    lcs_str = ""
+    i, j = m, n
+    while i > 0 and j > 0:
+        if X[i - 1] == Y[j - 1]:
+            lcs_str += X[i - 1]
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+
+    return lcs_str[::-1]  # 反转字符串得到正确的顺序
+
+
+if __name__ == "__main__":
+    X = "AGGTAB"
+    Y = "AGXTXAYB"
+    print("最长公共子序列：", lcs(X, Y))
